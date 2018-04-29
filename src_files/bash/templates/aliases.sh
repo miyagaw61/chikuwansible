d14a9c2fd2963435a86b81bd590fc102f14ad6d1
miyagaw61 <miyagaw61@gmail.com>
2018-04-29 23:35:10 +0900

    Move aliases.sh

---
+++ src_files/bash/templates/aliases.sh
@@ -0,0 +1,34 @@
+# set default useful options
+# ====================================
+alias diff="{{alias_diff}}"
+alias objdump="{{alias_objdump}}"
+alias less="{{alias_less}}"
+
+# be short
+# ====================================
+alias a="{{alias_a}}"
+alias pbcopy="{{alias_pbcopy}}"
+alias xl="{{alias_xl}}"
+alias pt="{{alias_pt}}"
+alias all-update="{{alias_all_update}}"
+alias op="{{alias_op}}"
+
+# alias ls
+# ====================================
+alias ls="{{alias_ls}}"
+alias ll="{{alias_ll}}"
+
+# alias source bashrc
+# ====================================
+alias soba="{{alias_soba}}"
+
+# my config
+# ====================================
+alias vim="{{alias_vim}}"
+alias ns="{{alias_ns}}"
+alias sl="{{alias_sl}}"
+
+# alias rusgit
+# ====================================
+# ANSIBLE_RUSGIT:{{rusgit_init}}
+# ANSIBLE_RUSGIT:alias rs="{{alias_rs}}"
