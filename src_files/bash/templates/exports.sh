1a21d85055bf7d9807825109e41870ce04488538
miyagaw61 <miyagaw61@gmail.com>
2018-04-29 23:34:52 +0900

    Move exports.sh

---
+++ src_files/bash/templates/exports.sh
@@ -0,0 +1,15 @@
+export DISPLAY=localhost:0.0
+export TERM=xterm-256color
+export REPOS="{{repos}}"
+export MY_REPOS="{{my_repos}}"
+export XDG_CONFIG_HOME="{{config_files}}"
+export PATH=$PATH:/usr/local/bin
+export GIT_NAME="{{git_user}}"
+export GIT_EMAIL="{{git_email}}"
+export NVIM_LISTEN_ADDRESS=/tmp/nvimsocket
+export BROWSER="{{browser}}"
+export RUST_SRC_PATH="{{rust_src_path}}"
+export EDITOR="{{editor}}"
+export RUSGIT_EDITOR="{{rusgit_editor}}"
+export GRAVE_ROOT="{{grave_root}}"
+export CARGO_PATH="{{cargo_path}}"
