bd144068fb54ba573102934fdb2402418703984b
miyagaw61 <miyagaw61@gmail.com>
2018-05-28 00:30:17 +0900

    Use pyenv_root instead of home

--- install_python.sh
+++ install_python.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if test ! $# -eq 2 ;then
-  echo "Usage: $0 HOME PYTHON_VERSION"
+  echo "Usage: $0 PYENV_ROOT PYTHON_VERSION"
 else
-  export PYENV_ROOT=$1/.pyenv
+  export PYENV_ROOT=$1
   export PATH=$PYENV_ROOT/bin:$PATH
   eval "$(pyenv init -)"
   pyenv install $2
