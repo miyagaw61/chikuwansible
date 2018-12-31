7e60a3d61f51f41802bc78e1c32ffb9266722374
miyagaw61 <miyagaw61@gmail.com>
2018-12-31 12:02:22 +0900

    Add u2 config if jp_flag to n_slack

--- src_files/keyhac/config.py
+++ src_files/keyhac/config.py
@@ -503,6 +503,9 @@ def configure(keymap):
         def n_slack(keymap_class):
             keymap_class["C-N"] = "S-A-Down"
             keymap_class["C-U0-N"] = "S-A-Up"
+            if jp_flag:
+                keymap_class["U2-N"] = "S-A-Down"
+                keymap_class["U2-S-N"] = "S-A-Up"
         
         def n_notepad(keymap_class):
             keymap_class["C-N"] = "Down"
