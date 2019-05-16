c5ccd4bbbab647989790f49739ace3e96be04671
miyagaw61 <miyagaw61@gmail.com>
2019-05-17 04:47:40 +0900

    Fix bug when there is a space in the path

--- install_python.sh
+++ install_python.sh
@@ -2,8 +2,8 @@
 if test ! $# -eq 2 ;then
   echo "Usage: $0 PYENV_ROOT PYTHON_VERSION"
 else
-  export PYENV_ROOT=$1
-  export PATH=$PYENV_ROOT/bin:$PATH
+  export PYENV_ROOT="$1"
+  export PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init -)"
   pyenv install $2
 fi
