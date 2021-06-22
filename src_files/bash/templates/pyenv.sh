65449bdec4cd4d04c4f7a967cdea8eb88a756271
miyase256 <miyagaw61@gmail.com>
2021-06-23 04:43:21 +0900

    follow pyenv update

--- src_files/bash/templates/pyenv.sh
+++ src_files/bash/templates/pyenv.sh
@@ -2,3 +2,7 @@
 #ANSIBLE_PYENVexport PYENV_ROOT={{pyenv_root}}
 #ANSIBLE_PYENVexport PATH=$PYENV_ROOT/bin:$PATH
 #ANSIBLE_PYENVeval "$(pyenv init -)"
+#ANSIBLE_PYENVeval "$(pyenv init --path)"
+#ANSIBLE_PYENVif command -v pyenv 1>/dev/null 2>&1; then
+#ANSIBLE_PYENV    eval "$(pyenv init -)"
+#ANSIBLE_PYENVfi
